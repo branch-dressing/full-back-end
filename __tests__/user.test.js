a12e9e9dc3fccaac576964f746b0b363f887066f
joelpdurham <joeldur@gmail.com>
2019-12-20 13:43:46 -0800

    wrote failing test for create user

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -17,4 +17,17 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('can create a user with a path', async() => {
+    return request(app)
+      .post('api/vi/auth/signup')
+      .send({ email: 'joel@joel.com', password: '1234' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'joel@joel.com',
+          __v: 0
+        });
+      });
+  });
 });
