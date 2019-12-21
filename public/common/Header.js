73e0a0020134e390b8c3c6801ac9ca9899a215f3
joelpdurham <joeldur@gmail.com>
2019-12-21 13:17:33 -0800

    setting up front end

---
+++ public/common/Header.js
@@ -0,0 +1,12 @@
+import Component from '../component.js';
+
+class Header extends Component {
+  renderHTML() {
+    return /*html*/`
+      <header>
+        <h1>Digital Bookshelf</h1>
+      </header>`;
+  }
+}
+
+export default Header;
---
+++ public/home/App.js
@@ -0,0 +1,26 @@
+import Component from '../component.js';
+import Header from '../common/header.js';
+import Signup from './Signup.js';
+import Login from './Login.js';
+
+class App extends Component {
+  onRender(el) {
+    const header = new Header();
+    el.prepend(header.renderDOM());
+
+    //the following needs to be in an if/else statement, only one should show at a time.
+    const signup = new Signup();
+    el.appendChild(signup.renderDOM());
+
+    const login = new Login();
+    el.appendChild(login.renderDOM());
+  }
+
+  renderHTML() {
+    return /*html*/`
+            <div>
+            <div>`;
+  }
+}
+
+export default App;
