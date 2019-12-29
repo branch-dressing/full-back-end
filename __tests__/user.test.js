a13f23a4306c43340719b8bdcfcb2483ac7e4cfe
joelpdurham <joeldur@gmail.com>
2019-12-28 21:45:56 -0800

    adjusted app secret

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
-
 const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const User = require('../lib/models/User');
-
 describe('app routes', () => {
   beforeAll(() => {
     connect();
